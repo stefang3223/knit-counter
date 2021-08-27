126145faeb2533a1e3500e7f7f1d070d1a14e00b
Stefan Gladkov <stefan.gladkov@comcarde.com>
2021-08-27 21:38:34 +0100

    my first ocmmit

---
+++ layouts/default.vue
@@ -0,0 +1,51 @@
+<template>
+
+
+<section class="hero is-link is-fullheight">
+  <div class="hero-body">
+    <div class="">
+            <p class="title has-text-centered">
+              Knit Counter
+            </p>
+      <p class="title has-text-centered">
+        {{counter}}
+      </p>
+      <p class="subtitle">
+        <b-button type="is-success" @click="counter++">Increase</b-button>
+      <b-button type="is-danger" @click="counter--">Decrease</b-button>     
+      </p>
+    </div>
+  </div>
+  
+  
+  
+</section>
+
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      counter: 0,
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
